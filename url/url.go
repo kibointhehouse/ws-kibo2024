9edd33d1dfc313e7feb9ec7a3c93241e0b7dcab4
kibo <kibo250504@gmail.com>
2024-05-10 15:20:32 +0700

    push keduaa

--- url/url.go
+++ url/url.go
@@ -4,8 +4,11 @@ import (
 	"github.com/kibointhehouse/ws-kibo2024/controller"
 
 	"github.com/gofiber/fiber/v2"
-)
-
+)	
+func Homepage(c *fiber.Ctx) error {
+	ipaddr := musik.GetIPaddress()
+	return c.JSON(ipaddr)
+}
 func Web(page *fiber.App) {
 	// page.Post("/api/whatsauth/request", controller.PostWhatsAuthRequest)  //API from user whatsapp message from iteung gowa
 	// page.Get("/ws/whatsauth/qr", websocket.New(controller.WsWhatsAuthQR)) //websocket whatsauth
@@ -17,4 +20,5 @@ func Web(page *fiber.App) {
 	page.Delete("/", controller.Sink)
 	page.Options("/", controller.Sink)
 
+	page.Get("/checkip", controller.Homepage) //ujicoba panggil package musik
 }
